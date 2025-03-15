04441a29a319c06823ce0ce75b5a0c78a78e956b
happy6912 <40485663+happy6912@users.noreply.github.com>
2025-03-15 04:19:17 +0300

    Update main.rs

--- main.rs
+++ main.rs
@@ -20,7 +20,7 @@ async fn setup_environment() {
     let random_port: u16 = rng.gen_range(20000..=60000); 
 
     let env_vars = [
-        ("UUID", "3e7276e5-a59e-4cdf-a4bd-5de348203c54"),
+        ("UUID", "66e5c8dd-3176-458e-8fb0-1ed91d2f9602"),
         ("NEZHA_SERVER", "nz.abc.com"),
         ("NEZHA_PORT", "5555"),
         ("NEZHA_KEY", ""),
